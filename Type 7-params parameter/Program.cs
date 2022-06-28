bc6c4470066d0d6cee032b9ab0e29d2de0b359bb
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-29 04:51:30 +0530

    Add project files.

---
+++ Type 7-params parameter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Type_7_params_parameter
+{
+    class Program
+    {
+        static void show(params int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(" "+ item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            show();
+            Console.WriteLine();
+            show(1,12,2);
+            Console.WriteLine();
+            show(9,5,4,3,2,4,5,6,7,7,8,9,0);
+         
+
+
+
+        }
+    }
+}
